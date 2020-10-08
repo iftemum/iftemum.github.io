52c0a6e78e966d37741619790fe78e112a7cf4fe
iftemum <iftemumraian@gmail.com>
2020-10-08 11:13:41 -0600

    update

--- Tic Tac Toe/sketch.js
+++ Tic Tac Toe/sketch.js
@@ -1,5 +1,4 @@
 let button;
-let w = 150;
 let board = [
   ["", "", ""],
   
@@ -20,6 +19,8 @@ let players = ["X", "O"];
 let cellWidth;
 let cellHeight;
 
+let w = width/3;
+let h = height/3;
 
 
 function setup() {
@@ -31,33 +32,33 @@ function setup() {
 function draw() {
   // startButton();
   background(220);
-  displayGrid();
-  // draw_board();
+  // displayGrid();
+  drawBoard();
 }
 
 
-function displayGrid(){
-  for(let y = 0; y <3; y++){
-    for (let x = 0; x<3; x++){
+// function displayGrid(){
+//   for(let y = 0; y <3; y++){
+//     for (let x = 0; x<3; x++){
       
-      let i;
-      let j;
-      stroke(0);
-      rect(i,j, width/3, height/3);
-      i += 100;
-      j += 100;
+//       let i;
+//       let j;
+//       stroke(0);
+//       rect(i,j, width/3, height/3);
+//       i += 100;
+//       j += 100;
 
-    }
-  }
-}
-
-
-// function draw_board() {
-//   line(0,w,w*3,w);
-//   line(0,w*2,w*3,w*2);
-//   line(w,0,w,w*3);
-//   line(w*2,0,w*2,w*3);
+//     }
+//   }
 // }
+
+
+function drawBoard() {
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+}
 
 
 // function startButton(){
